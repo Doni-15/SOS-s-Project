e4c0de55f3553762f4caa3d9a05d06b5e28b0061
Doni Rivaldo Simamora <donisimamora15@gmail.com>
2026-05-29 21:05:53 +0700

    inisialisasi awal frontend SOS

---
+++ SelfOrderSystemManagement_Project_v1.0/sos_frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-100 flex items-center justify-center">
+      <div className="rounded-2xl bg-white p-8 shadow-lg">
+        <h1 className="text-3xl font-bold text-slate-900">
+          Self-Order System
+        </h1>
+        <p className="mt-2 text-slate-500">
+          React + Vite + Tailwind siap dipakai.
+        </p>
+        <button className="mt-5 rounded-xl bg-orange-500 px-5 py-3 font-semibold text-white hover:bg-orange-600">
+          Powerred By: MBG - Mas Bahlil Ganteng.
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
